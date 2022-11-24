fa767142649c4dedc8b22b9ba97f6052d72ec973
Shan15Dev <shansai@bluewin.ch>
2022-11-24 11:13:52 +0100

    init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a = 2
+
+func main() {
+	if a == 3 {
+		fmt.Println(a)
+	} else {
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	}
+
+    switch a {
+    case 1:
+        fmt.Println("One")
+    case 2:
+        fmt.Println("Two")
+
+    }
+
+    array := [5]int{4, 5, 6, 7, 8}
+
+    // Itterate through list
+    for s := range array {
+        fmt.Println(s)
+    }
+
+    // Itterate through list with giving the current position
+    for i, s := range array {
+        fmt.Println(i, s)
+    }
+}
