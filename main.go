aeb8e445234342d7789938b67575cc1d645baff3
Shan15Dev <shansai@bluewin.ch>
2022-11-24 11:55:53 +0100

    Go slices

--- main.go
+++ main.go
@@ -15,23 +15,40 @@ func main() {
 		}
 	}
 
-    switch a {
-    case 1:
-        fmt.Println("One")
-    case 2:
-        fmt.Println("Two")
+	switch a {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
 
-    }
-s
-    array := [5]int{4, 5, 6, 7, 8}
+	}
 
-    // Itterate through list
-    for s := range array {
-        fmt.Println(s)
-    }
+	array := [5]int{4, 5, 6, 7, 8}
 
-    // Itterate through list with giving the current position
-    for i, s := range array {
-        fmt.Println(i, s)
-    }
+	// Itterate through list with giving the current position
+	for i, s := range array {
+		fmt.Println(i, s)
+	}
+
+	array2 := [...]int{4, 5, 6, 7, 8, 234123, 412341234, 12341234, 1234, 123, 41, 23}
+
+	for i, s := range array2 {
+		fmt.Println(i, s)
+	}
+	// Why does that not work?
+	/*
+	   for s := range array2 {
+	       fmt.Println(s)
+	   }
+	*/
+
+	var cars = [4]string{"Switzerland", "Germany", "France"}
+	fmt.Println(cars)
+	fmt.Println(cars[3])
+
+	var carsMake = make([]string, 0, 10)
+    carsMake = append(carsMake, "Test")
+    fmt.Println(len(carsMake))
+	fmt.Println(cap(carsMake))
+	fmt.Println(carsMake)
 }
