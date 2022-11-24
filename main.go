4b064d2c1df00976677863f56227cb5d11c89534
Shan15Dev <shansai@bluewin.ch>
2022-11-24 13:22:31 +0100

    Multi-switch-case

--- main.go
+++ main.go
@@ -59,13 +59,50 @@ func main() {
 		fmt.Println("Ou. Well... Let's work for the 5 or higher next time.")
 	}
 
+	// Nested if
 	grade = 6
 	if grade >= 5 {
-        fmt.Printf("Your grade is 5 or higher: %d", grade)
+		fmt.Printf("Your grade is 5 or higher: %d", grade)
+		fmt.Println()
 		if grade > 5 {
-            fmt.Printf("Your grade higher then 5:  %d", grade)
+			fmt.Printf("Your grade higher then 5:  %d", grade)
+			fmt.Println()
 		}
 	} else {
-        fmt.Printf("Your grade is lower than 5: %d", grade)
+		fmt.Printf("Your grade is lower than 5: %d", grade)
+		fmt.Println()
 	}
+
+	grade = 7
+    // Switch-Case switch
+    // Can only be one datatype. SO I cannot put a number in the first case and a string in the second case.
+	switch grade {
+	case 1:
+		fmt.Println("Your grade is", grade)
+
+	case 2:
+		fmt.Println("Your grade is", grade)
+	case 3:
+		fmt.Println("Your grade is", grade)
+	case 4:
+		fmt.Println("Your grade is", grade)
+	case 5:
+		fmt.Println("Your grade is", grade)
+	case 6:
+		fmt.Println("Your grade is", grade)
+	default:
+		fmt.Println("Your grade is over the number 6. Your input was:", grade)
+	}
+
+    // Multi-case switch
+
+    day := 4
+    switch day{
+    case 1, 2, 3, 4, 5:
+        fmt.Println("It is a weekday")
+    case 6, 7:
+        fmt.Println("It is WEEKEND :)")
+    default:
+        fmt.Println("After Sunday, there is Monday. There is no", day, "th day")
+    }
 }
